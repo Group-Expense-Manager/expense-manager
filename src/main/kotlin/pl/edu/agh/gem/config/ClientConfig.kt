346c01da472c02fd5663614e3cef5f0dd6bd930b
Jakub Spolnik <lawos231@gmail.com>
2024-08-25 22:01:32 +0200

    GEM-160 | Add auto logs

--- src/main/kotlin/pl/edu/agh/gem/config/ClientConfig.kt
+++ src/main/kotlin/pl/edu/agh/gem/config/ClientConfig.kt
@@ -6,35 +6,48 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import pl.edu.agh.gem.helper.http.GemRestTemplateFactory
 import java.time.Duration
 
 @Configuration
 class ClientConfig {
     @Bean
     @Qualifier("GroupManagerRestTemplate")
-    fun groupManagerRestTemplate(groupManagerProperties: GroupManagerProperties): RestTemplate {
-        return RestTemplateBuilder()
-            .setConnectTimeout(groupManagerProperties.connectTimeout)
-            .setReadTimeout(groupManagerProperties.readTimeout)
-            .build()
+    fun groupManagerRestTemplate(
+        groupManagerProperties: GroupManagerProperties,
+        gemRestTemplateFactory: GemRestTemplateFactory,
+    ): RestTemplate {
+        return gemRestTemplateFactory
+                .builder()
+                .withReadTimeout(groupManagerProperties.readTimeout)
+                .withConnectTimeout(groupManagerProperties.connectTimeout)
+                .build()
     }
 
     @Bean
     @Qualifier("CurrencyManagerRestTemplate")
-    fun currencyManagerRestTemplate(currencyManagerProperties: CurrencyManagerProperties): RestTemplate {
-        return RestTemplateBuilder()
-            .setConnectTimeout(currencyManagerProperties.connectTimeout)
-            .setReadTimeout(currencyManagerProperties.readTimeout)
-            .build()
+    fun currencyManagerRestTemplate(
+        currencyManagerProperties: CurrencyManagerProperties,
+        gemRestTemplateFactory: GemRestTemplateFactory,
+    ): RestTemplate {
+        return gemRestTemplateFactory
+                .builder()
+                .withReadTimeout(currencyManagerProperties.readTimeout)
+                .withConnectTimeout(currencyManagerProperties.connectTimeout)
+                .build()
     }
 
     @Bean
     @Qualifier("AttachmentStoreRestTemplate")
-    fun attachmentStoreRestTemplate(attachmentStoreProperties: AttachmentStoreProperties): RestTemplate {
-        return RestTemplateBuilder()
-            .setConnectTimeout(attachmentStoreProperties.connectTimeout)
-            .setReadTimeout(attachmentStoreProperties.readTimeout)
-            .build()
+    fun attachmentStoreRestTemplate(
+        attachmentStoreProperties: AttachmentStoreProperties,
+        gemRestTemplateFactory: GemRestTemplateFactory,
+    ): RestTemplate {
+        return gemRestTemplateFactory
+                .builder()
+                .withReadTimeout(attachmentStoreProperties.readTimeout)
+                .withConnectTimeout(attachmentStoreProperties.connectTimeout)
+                .build()
     }
 }
 
